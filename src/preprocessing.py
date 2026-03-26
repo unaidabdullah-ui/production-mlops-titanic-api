6982207c56a153a8b310af83f21dd5ac74231e22
Unaid <unaid@UNAIDABDULLAH.localdomain>
2026-03-26 19:39:03 +0000

    Initial commit - MLOps Titanic API with Docker

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,19 @@
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def preprocessing(df):
-    df =df.dropna()
+    # Drop missing values
+    df = df.dropna()
 
-    x = df.drop("Survived",axis=1)
-    y =df["Survived"]
-    return train_test_split(x,y,test_size=0.2,random_state=42)
+    # Drop unnecessary columns
+    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
+
+    # Features and target
+    x = df.drop('Survived', axis=1)
+    y = df['Survived']
+
+    # Convert categorical to numeric
+    x = pd.get_dummies(x, columns=['Sex', 'Embarked'], drop_first=True)
+
+    # Train-test split
+    return train_test_split(x, y, test_size=0.2, random_state=42)
--- src/train.py
+++ src/train.py
@@ -4,24 +4,29 @@ from sklearn.ensemble import RandomForestClassifier
 from ingestion import load_data
 from preprocessing import preprocessing
 import joblib
+import os
 
 mlflow.set_experiment("Titanic Survival Prediction")
 
 def train():
     df = load_data()
-    x_train,x_test,y_train,y_test = preprocessing(df)
+    x_train, x_test, y_train, y_test = preprocessing(df)
 
     model = RandomForestClassifier()
-    model.fit(x_train,y_train)
+    model.fit(x_train, y_train)
 
-    acc = model.score(x_test,y_test)
+    acc = model.score(x_test, y_test)
 
-    joblib.dump("model/model.pkl")
+    # Create folder if not exists
+    os.makedirs("model", exist_ok=True)
+
+    # Save model
+    joblib.dump(model, "model/model.pkl")
 
     with mlflow.start_run():
-        mlflow.log_param("model","RandomForestClassifier")
-        mlflow.log_metric("accuracy",acc)
-        mlflow.sklearn.log_model(model,"model")
+        mlflow.log_param("model", "RandomForestClassifier")
+        mlflow.log_metric("accuracy", acc)
+        mlflow.sklearn.log_model(model, "model")
 
     print(f"Model Accuracy: {acc}")
 
